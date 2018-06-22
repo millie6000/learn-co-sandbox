7ba77715737ae035d506f73308a3b2cd6c3fe3f8
millie6000 <alohaturtle2003@gmail.com>
2018-06-22 21:16:26 +0000

    automatically backed up by learn

--- user.rb
+++ user.rb
@@ -18,11 +18,17 @@ end
   # puts "#{millie.name_of_person} posted her story #{millie.story_of_person} and her password is #{millie.password_of_person}"
   
   # caroline = Snapchat.new("caroline23","in jamiaca",)
-  puts "whats your user name?" 
-  name = gets.chomp 
-  puts "that's a cool #{name}! whats your password?"
-  password=gets.chomp 
-  puts"#{password}, is a weird password. were do you want to be in your story?"
+  # puts "whats your user name?" 
+  # name = gets.chomp 
+  # puts "that's a cool #{name}! whats your password?"
+  # password=gets.chomp 
+  # puts"#{password}, is a weird password. were do you want to be in your story?"
+  # puts "okay thanks, this is your profile"
+  # story=gets.chomp
+  # sleep(1)
+  # puts "#{name} 
+  # #{story}
+  # forgot your password"
   
   
   
